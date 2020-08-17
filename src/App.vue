96345547bd9f390359c1ee4ffe3eb3987a425eb0
Nikolaj Frey <lopudesigns@gmail.com>
2020-08-17 18:46:18 +1000

    Added hidden developer message

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,13 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+	created(){
+		console.log(
+			`%cWant to be developer friends? Send me a message either on twitter @lopuai or nikolaj@alopu.com\nI don't have m/any developer friends, I live in Melbourne, Australia, really into things, graphs, and UI's :)`,
+			"background: #1B2327 ; color: #FFC14C"
+			)
+	}
 }
 </script>
 
