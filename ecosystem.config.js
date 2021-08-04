377454acfa08ef7cb0bc2e91acc86f7b4c2c45c4
Lopu <lopu@ip-172-31-34-179.ap-southeast-2.compute.internal>
2021-08-04 10:56:28 +0000

    dev

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,8 @@
 module.exports = {
-  apps : [{
-		name: 'lopudesigns.com',
-    script: 'npm',
-		args: 'run serve',
-    watch: '.'
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  apps : [
+	{
+		name: 'prod-serve-lopudesigns',
+    		script: 'node serve.js'
+  	}
+  ]
 };
